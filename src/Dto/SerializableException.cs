d921428127c35f2b46e539160e75e177c84d64d0
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-04-11 01:23:37 -0600

    starting the serializable exception

---
+++ src/Dto/SerializableException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace StandardDot.Dto
+{
+    public class SerializableException
+    {
+        public SerializableException(Exception exception)
+        {
+            HelpLink = exception.HelpLink;
+        }
+
+        [DataMember(Name = "helpLink")]
+        public string HelpLink { get; set; }
+    }
+}
