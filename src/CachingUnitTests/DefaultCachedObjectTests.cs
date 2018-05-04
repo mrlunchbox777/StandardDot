a4e4bcbfeba1d67ac276adeb245458eaf733a937
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-05-03 21:14:11 -0600

    Adding the cached object tests

---
+++ src/CachingUnitTests/DefaultCachedObjectTests.cs
@@ -0,0 +1,27 @@
+using System;
+using StandardDot.Abstract.Caching;
+using StandardDot.Caching;
+using Xunit;
+
+namespace CachingUnitTests
+{
+    public class DefaultCachedObjectTests
+    {
+        [Fact]
+        public void Properties()
+        {
+            DefaultCachedObject<string> cachedObject = new DefaultCachedObject<string>();
+            const string value = "a value";
+            DateTime time = DateTime.UtcNow;
+            DateTime expireTime = DateTime.UtcNow.AddMinutes(5);
+
+            cachedObject.ExpireTime = expireTime;
+            cachedObject.CachedTime = time;
+            cachedObject.Value = value;
+
+            Assert.Equal(expireTime, cachedObject.ExpireTime);
+            Assert.Equal(time, cachedObject.CachedTime);
+            Assert.Equal(value, cachedObject.Value);
+        }
+    }
+}
