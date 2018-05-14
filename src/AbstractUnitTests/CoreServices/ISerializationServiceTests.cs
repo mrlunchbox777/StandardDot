8a5e587c75b59b6a002fe57f4bc7ecf0511181e6
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-05-13 21:23:40 -0600

    Finished off the testing

--- src/AbstractUnitTests/CoreServices/ISerializationServiceTests.cs
+++ src/AbstractUnitTests/CoreServices/ISerializationServiceTests.cs
@@ -1,3 +1,7 @@
+using System.IO;
+using Moq;
+using StandardDot.Abstract.CoreServices;
+using StandardDot.TestClasses;
 using Xunit;
 
 namespace AbstractUnitTests.CoreServices
@@ -5,7 +9,35 @@ namespace AbstractUnitTests.CoreServices
     public class ISerializationServiceTests
     {
         [Fact]
-        public void NotATest()
-        {}
+        public void SerializeObjectTest()
+        {
+            string expected = "expected";
+            Foobar foobar = new Foobar { Foo = 4, Bar = 9 };
+            Mock<ISerializationService> serializationServiceProxy = new Mock<ISerializationService>(MockBehavior.Strict);
+            serializationServiceProxy.Setup(x => x.SerializeObject(foobar)).Returns(expected);
+            Assert.Equal(expected, serializationServiceProxy.Object.SerializeObject(foobar));
+        }
+        
+        [Fact]
+        public void DeserializeObjectTest()
+        {
+            string expected = "expected";
+            Foobar foobar = new Foobar { Foo = 4, Bar = 9 };
+            Mock<ISerializationService> serializationServiceProxy = new Mock<ISerializationService>(MockBehavior.Strict);
+            serializationServiceProxy.Setup(x => x.DeserializeObject<Foobar>(expected)).Returns(foobar);
+            Assert.Equal(foobar, serializationServiceProxy.Object.DeserializeObject<Foobar>(expected));
+        }
+        
+        [Fact]
+        public void DeserializeObjectStreamTest()
+        {
+            using(Stream expected = new MemoryStream())
+            {
+                Foobar foobar = new Foobar { Foo = 4, Bar = 9 };
+                Mock<ISerializationService> serializationServiceProxy = new Mock<ISerializationService>(MockBehavior.Strict);
+                serializationServiceProxy.Setup(x => x.DeserializeObject<Foobar>(expected)).Returns(foobar);
+                Assert.Equal(foobar, serializationServiceProxy.Object.DeserializeObject<Foobar>(expected));
+            }
+        }
     }
 }
---
+++ src/AbstractUnitTests/CoreServices/LogBaseEnumerableBaseTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Moq.Protected;
+using StandardDot.Abstract.CoreServices;
+using StandardDot.Dto.CoreServices;
+using Xunit;
+
+namespace AbstractUnitTests.CoreServices
+{
+    public class LogBaseEnumerableBaseTests
+    {
+        [Fact]
+        public void ConstructorTests()
+        {
+            IEnumerable<LogBase> source = new List<LogBase> { new LogBase() };
+            Mock<LogBaseEnumerableBase> enumerable = new Mock<LogBaseEnumerableBase>(MockBehavior.Loose, source);
+            enumerable.CallBase = true;
+
+            Assert.Single(enumerable.Object);
+            Assert.Equal(source.Single(), enumerable.Object.Single());
+
+            Mock<LogBaseEnumerableBase> enumerable2 = new Mock<LogBaseEnumerableBase>(MockBehavior.Loose, enumerable.Object);
+            enumerable2.CallBase = true;
+
+            Assert.Single(enumerable2.Object);
+            Assert.Equal(source.Single(), enumerable2.Object.Single());
+        }
+
+        [Fact]
+        public void GetGenericEnumeratorTest()
+        {
+            IEnumerable<LogBase> source = new List<LogBase> { new LogBase() };
+            Mock<LogBaseEnumerableBase> enumerable = new Mock<LogBaseEnumerableBase>(MockBehavior.Loose, source);
+            enumerable.CallBase = true;
+
+            using(IEnumerator<LogBase> enumerator = enumerable.Object.GetEnumerator()) {
+                Assert.Null(enumerator.Current);
+                Assert.True(enumerator.MoveNext());
+                Assert.Equal(source.Single(), enumerator.Current);
+                Assert.False(enumerator.MoveNext());
+                Assert.Null(enumerator.Current);
+            }
+        }
+
+        [Fact]
+        public void GetEnumeratorTest()
+        {
+            IEnumerable<LogBase> source = new List<LogBase> { new LogBase() };
+            Mock<LogBaseEnumerableBase> enumerable = new Mock<LogBaseEnumerableBase>(MockBehavior.Loose, source);
+            enumerable.CallBase = true;
+
+            IEnumerator enumerator = ((IEnumerable)enumerable.Object).GetEnumerator();
+            Assert.Throws<InvalidOperationException>(() => enumerator.Current);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(source.Single(), enumerator.Current);
+            Assert.False(enumerator.MoveNext());
+            Assert.Throws<InvalidOperationException>(() => enumerator.Current);
+        }
+    }
+}
