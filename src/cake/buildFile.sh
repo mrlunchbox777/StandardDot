0f8144543f3a16078982d98c44fc8c38bbbd6d15
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-06-27 22:07:59 -0600

    making sure the tools dir always gets cleaned

--- src/cake/buildFile.sh
+++ src/cake/buildFile.sh
@@ -156,6 +156,9 @@ startRunning()
     diff=()
     if [ -z "$CI_COMMIT_REF_NAME" ] || [[ "$CI_COMMIT_REF_NAME" != "master" && "$CI_COMMIT_REF_NAME" != "develop" ]]; then
         echo "Not a building branch... exiting"
+        if [ -d "$TOOLS_DIR" ]; then
+            rm -rf "$TOOLS_DIR"
+        fi
         exit
     fi
 
