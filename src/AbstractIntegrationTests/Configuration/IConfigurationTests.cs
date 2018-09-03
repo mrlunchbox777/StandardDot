5d8876e4ff043d08c914fef80a0df93e5f565068
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-09-03 14:31:48 -0600

    Adding the abstract integration tests

---
+++ src/AbstractIntegrationTests/Configuration/IConfigurationTests.cs
@@ -0,0 +1,19 @@
+using StandardDot.Abstract.Configuration;
+using StandardDot.TestClasses.TestConfigurationMetadatas;
+using StandardDot.TestClasses.TestConfigurations;
+using Xunit;
+
+namespace StandardDot.Abstract.IntegrationTests.Configuration
+{
+    public class IConfigurationTests
+    {
+        [Fact]
+        public void Properties()
+        {
+            IConfiguration<TestConfiguration, TestConfigurationMetadata> configuration = new TestConfiguration();
+            Assert.Null(configuration.ConfigurationMetadata);
+            configuration.ConfigurationMetadata = new TestConfigurationMetadata();
+            Assert.NotNull(configuration.ConfigurationMetadata);
+        }
+    }
+}
