d6b3fa4a5dcfffa3ae42bd8e3486e38ad0054abc
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-09-22 13:24:08 -0600

    Just using serialization instead

--- src/CachingRedis/Dto/RedisId.cs
+++ src/CachingRedis/Dto/RedisId.cs
@@ -1,25 +1,23 @@
-using System;
-using System.Linq;
+using System.Runtime.Serialization;
 using StandardDot.Caching.Redis.Enums;
+using StandardDot.CoreExtensions;
+using StandardDot.CoreExtensions.Object;
 
 namespace StandardDot.Caching.Redis.Dto
 {
+	[DataContract]
 	public class RedisId
 	{
-		protected internal RedisId(){}
-		public RedisId(RedisServiceType serviceType, string objectIdentifier, string hashSetIdentifier = null)
-		{
-			ServiceType = serviceType;
-			ObjectIdentifier = objectIdentifier;
-			HashSetIdentifier = hashSetIdentifier;
-		}
-		private static readonly string KeySeperator = "---___+++";
+		[DataMember(Name = "hashSetIdentifier")]
 		public string HashSetIdentifier { get; set; }
 
+		[DataMember(Name = "objectIdentifier")]
 		public string ObjectIdentifier { get; set; }
 
+		[DataMember(Name = "serviceType")]
 		public RedisServiceType ServiceType { get; set; }
 
+		[IgnoreDataMember]
 		public string FullKey
 		{
 			get
@@ -35,75 +33,7 @@ namespace StandardDot.Caching.Redis.Dto
 			}
 		}
 
-		public string CacheKey
-		{
-			get
-			{
-				string intro = ServiceType + KeySeperator;
-				if (!HasFullKey)
-				{
-					return "";
-				}
-				switch (ServiceType)
-				{
-					case RedisServiceType.HashSet:
-						return intro + (HashSetIdentifier ?? "") + KeySeperator + (ObjectIdentifier ?? "");
-					case RedisServiceType.KeyValue:
-					default:
-						return intro + (ObjectIdentifier ?? "");
-				}
-			}
-		}
-
-		public static implicit operator string(RedisId id)
-		{
-			return id?.CacheKey;
-		}
-
-		public static implicit operator RedisId(string id)
-		{
-			RedisId newId = new RedisId();
-			if (string.IsNullOrWhiteSpace(id))
-			{
-				return newId;
-			}
-			if (!id.Contains(KeySeperator))
-			{
-				newId.ObjectIdentifier = id;
-				newId.ServiceType = RedisServiceType.KeyValue;
-				return newId;
-			}
-			string[] parts = id.Split(new[] {KeySeperator}, StringSplitOptions.None);
-			if (!(parts?.Any() ?? false))
-			{
-				newId.ObjectIdentifier = id;
-				newId.ServiceType = RedisServiceType.KeyValue;
-				return newId;
-			}
-			bool gotTheServiceType = Enum.TryParse(parts.First(), out RedisServiceType serviceType);
-			if (!gotTheServiceType)
-			{
-				newId.ObjectIdentifier = id;
-				newId.ServiceType = RedisServiceType.KeyValue;
-				return newId;
-			}
-			newId.ServiceType = serviceType;
-			switch (serviceType)
-			{
-				case RedisServiceType.HashSet:
-					newId.HashSetIdentifier = parts.Skip(1).Take(1).SingleOrDefault();
-					newId.ObjectIdentifier = parts.Skip(2).Aggregate("", (x, y) => x + y);
-					break;
-				case RedisServiceType.KeyValue:
-					newId.ObjectIdentifier = parts.Skip(1).Aggregate("", (x, y) => x + y);
-					break;
-				case RedisServiceType.Provider:
-				default:
-					newId.ServiceType = RedisServiceType.KeyValue;
-					goto case RedisServiceType.KeyValue;
-			}
-		}
-
+		[IgnoreDataMember]
 		public bool HasFullKey
 		{
 			get
@@ -117,6 +47,16 @@ namespace StandardDot.Caching.Redis.Dto
 						return !(string.IsNullOrWhiteSpace(ObjectIdentifier));
 				}
 			}
+		}
+
+		public static implicit operator string(RedisId id)
+		{
+			return id?.SerializeJson();
+		}
+
+		public static implicit operator RedisId(string id)
+		{
+			return id?.DeserializeJson<RedisId>();
 		}
 	}
 }
