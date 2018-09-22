91c73e05b7861cb719e3d1ed5e381fcaf58fa0d0
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-09-22 13:19:46 -0600

    First Pass at redisid string conversion

--- src/CachingRedis/Dto/RedisId.cs
+++ src/CachingRedis/Dto/RedisId.cs
@@ -1,9 +1,19 @@
+using System;
+using System.Linq;
 using StandardDot.Caching.Redis.Enums;
 
 namespace StandardDot.Caching.Redis.Dto
 {
 	public class RedisId
 	{
+		protected internal RedisId(){}
+		public RedisId(RedisServiceType serviceType, string objectIdentifier, string hashSetIdentifier = null)
+		{
+			ServiceType = serviceType;
+			ObjectIdentifier = objectIdentifier;
+			HashSetIdentifier = hashSetIdentifier;
+		}
+		private static readonly string KeySeperator = "---___+++";
 		public string HashSetIdentifier { get; set; }
 
 		public string ObjectIdentifier { get; set; }
@@ -22,6 +32,75 @@ namespace StandardDot.Caching.Redis.Dto
 					default:
 						return ObjectIdentifier ?? "";
 				}
+			}
+		}
+
+		public string CacheKey
+		{
+			get
+			{
+				string intro = ServiceType + KeySeperator;
+				if (!HasFullKey)
+				{
+					return "";
+				}
+				switch (ServiceType)
+				{
+					case RedisServiceType.HashSet:
+						return intro + (HashSetIdentifier ?? "") + KeySeperator + (ObjectIdentifier ?? "");
+					case RedisServiceType.KeyValue:
+					default:
+						return intro + (ObjectIdentifier ?? "");
+				}
+			}
+		}
+
+		public static implicit operator string(RedisId id)
+		{
+			return id?.CacheKey;
+		}
+
+		public static implicit operator RedisId(string id)
+		{
+			RedisId newId = new RedisId();
+			if (string.IsNullOrWhiteSpace(id))
+			{
+				return newId;
+			}
+			if (!id.Contains(KeySeperator))
+			{
+				newId.ObjectIdentifier = id;
+				newId.ServiceType = RedisServiceType.KeyValue;
+				return newId;
+			}
+			string[] parts = id.Split(new[] {KeySeperator}, StringSplitOptions.None);
+			if (!(parts?.Any() ?? false))
+			{
+				newId.ObjectIdentifier = id;
+				newId.ServiceType = RedisServiceType.KeyValue;
+				return newId;
+			}
+			bool gotTheServiceType = Enum.TryParse(parts.First(), out RedisServiceType serviceType);
+			if (!gotTheServiceType)
+			{
+				newId.ObjectIdentifier = id;
+				newId.ServiceType = RedisServiceType.KeyValue;
+				return newId;
+			}
+			newId.ServiceType = serviceType;
+			switch (serviceType)
+			{
+				case RedisServiceType.HashSet:
+					newId.HashSetIdentifier = parts.Skip(1).Take(1).SingleOrDefault();
+					newId.ObjectIdentifier = parts.Skip(2).Aggregate("", (x, y) => x + y);
+					break;
+				case RedisServiceType.KeyValue:
+					newId.ObjectIdentifier = parts.Skip(1).Aggregate("", (x, y) => x + y);
+					break;
+				case RedisServiceType.Provider:
+				default:
+					newId.ServiceType = RedisServiceType.KeyValue;
+					goto case RedisServiceType.KeyValue;
 			}
 		}
 
