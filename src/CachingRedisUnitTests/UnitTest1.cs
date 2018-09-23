1dd5841cc046e1bad510038b42fb77329f523fd4
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-09-23 13:15:55 -0600

    Fixing unit test whitespace

--- src/CachingRedisUnitTests/UnitTest1.cs
+++ src/CachingRedisUnitTests/UnitTest1.cs
@@ -3,12 +3,12 @@ using Xunit;
 
 namespace CachingRedisUnitTests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
 
-        }
-    }
+		}
+	}
 }
