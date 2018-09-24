af8ddc18850be89c502544802cce923a4a3e0ce5
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-09-23 18:50:43 -0600

    Forcing data contract

--- src/Caching/DefaultCachedObject.cs
+++ src/Caching/DefaultCachedObject.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.Serialization;
 using StandardDot.Abstract.Caching;
 
 namespace StandardDot.Caching
@@ -7,16 +8,22 @@ namespace StandardDot.Caching
 	/// A Basic Caching Object Wrapper
 	/// </summary>
 	/// <typeparam name="T">The wrapped object Type</typeparam>
+	[DataContract]
 	public class DefaultCachedObject<T> : ICachedObject<T>
 	{
+		[DataMember(Name = "cachedTime")]
 		public DateTime CachedTime { get; set; }
 
+		[DataMember(Name = "expireTime")]
 		public DateTime ExpireTime { get; set; }
 
+		[DataMember(Name = "value")]
 		public T Value { get; set; }
 
+		[DataMember(Name = "retrievedSuccesfully")]
 		public bool RetrievedSuccesfully { get; set; }
 
+		[IgnoreDataMember]
 		public object UntypedValue { get => Value; set => Value = (value is T ? (T)value : default(T)); }
 	}
 }
