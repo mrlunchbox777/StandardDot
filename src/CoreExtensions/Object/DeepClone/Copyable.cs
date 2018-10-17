05f37354b22ce97611a794c23293ea80fa17ee9e
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-10-17 10:54:00 -0600

    Updating how the selection is handled without an exact match

--- src/CoreExtensions/Object/DeepClone/Copyable.cs
+++ src/CoreExtensions/Object/DeepClone/Copyable.cs
@@ -52,6 +52,7 @@ namespace StandardDot.CoreExtensions.Object.DeepClone
 			constructor = allConstructors.FirstOrDefault(x => (x as MethodBase) == method)
 				?? allConstructors.FirstOrDefault(x => x.MetadataToken == method.MetadataToken);
 			constructorArgs = args;
+			// if we can match the constructor by token or equality, that's the one we need
 			if (constructor != null)
 			{
 				return;
@@ -94,7 +95,7 @@ namespace StandardDot.CoreExtensions.Object.DeepClone
 				{
 					argType = argType.IsGenericTypeDefinition ? argType.GetGenericTypeDefinition() : argType.UnderlyingSystemType;
 				}
-				if (!assumeCorrectBecauseGeneric && !argType.GetTypeInfo().IsAssignableFrom(parameterType.GetTypeInfo())
+				if (!assumeCorrectBecauseGeneric && !parameterType.GetTypeInfo().IsAssignableFrom(argType.GetTypeInfo())
 					&& (!(args[i] is Type && parameterType == typeof(Type))))
 				{
 					throw new InvalidOperationException(string.Format("Copyable constructed with invalid type {0} for argument #{2} (should be {1})",
