d628b305a8958678c1d46cbbf6ca45aaf39b9c35
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-11-06 12:40:32 -0700

    Adding a javascript error class

---
+++ src/Dto/Exception/JavaScriptError.cs
@@ -0,0 +1,76 @@
+using System.Runtime.Serialization;
+
+namespace StandardDot.Dto.Exception
+{
+	[DataContract]
+	public class JavaScriptError
+	{
+		public JavaScriptError() { }
+		
+		public JavaScriptError(string message, string name
+			// Mozilla
+			, string fileName = null, int? lineNumber = null, int? columnNumber = null, string stack = null
+			// Microsoft
+			, string description = null, int? number = null
+			// methods
+			, string errorSource = null, string toStringResult = null)
+		{
+		}
+
+		[DataMember(Name = "message")]
+		public string Message { get; set; }
+
+		[DataMember(Name = "name")]
+		public string Name { get; set; }
+
+		// Mozilla
+		[DataMember(Name = "fileName")]
+		public string FileName { get; set; }
+
+		[DataMember(Name = "lineNumber")]
+		public int? LineNumber { get; set; }
+		
+		[DataMember(Name = "columnNumber")]
+		public int? ColumnNumber { get; set; }
+		
+		[DataMember(Name = "stack")]
+		public string Stack { get; set; }
+		
+		// Microsoft
+		[DataMember(Name = "description")]
+		public string Description { get; set; }
+		
+		[DataMember(Name = "number")]
+		public int? Number { get; set; }
+
+		// Methods
+		[DataMember(Name = "errorSource")]
+		public string ErrorSource { get; set; }
+
+		[DataMember(Name = "toStringResult")]
+		public string ToStringResult { get; set; }
+
+		// Checks
+		[DataMember(Name = "hasMozilla")]
+		public bool HasMozilla
+		{
+			get => !string.IsNullOrWhiteSpace(FileName) || LineNumber != null || ColumnNumber != null || !string.IsNullOrWhiteSpace(Stack);
+			set {}
+		}
+		
+		[DataMember(Name = "hasMicrosoft")]
+		public bool HasMicrosoft
+		{
+			get => !string.IsNullOrWhiteSpace(Description) || Number != null;
+			set {}
+		}
+		
+		[DataMember(Name = "hasMethodValues")]
+		public bool HasMethodValues
+		{
+			get => !string.IsNullOrWhiteSpace(ErrorSource) || !string.IsNullOrWhiteSpace(ToStringResult);
+			set {}
+		}
+        
+    }
+}
