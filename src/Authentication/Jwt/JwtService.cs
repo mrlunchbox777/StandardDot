b5478fb1d4fb4d180ce887885f9c18f0317f4115
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-12-04 22:29:42 -0700

    Some JWT updates

--- src/Authentication/Jwt/JwtService.cs
+++ src/Authentication/Jwt/JwtService.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Security;
 using JWT;
 using JWT.Algorithms;
 using JWT.Serializers;
@@ -8,11 +9,23 @@ using StandardDot.Enums;
 
 namespace StandardDot.Authentication.Jwt
 {
+	/// <summary>
+	/// The base JWT Service Class that will handle JWT authentication
+	/// </summary>
 	public class JwtService
 	{
-		public JwtService(string secret, IJsonSerializer serializer, IDateTimeProvider provider, IJwtValidator validator,
+		/// <param name="secret">The secret used to encrypt the JWT</param>
+		/// <param name="serializer">The Json serialization service used to handle JWTs</param>
+		/// <param name="provider">The provider for DateTimes</param>
+		/// <param name="validator">The validation service for JWTs</param>
+		/// <param name="urlEncoder">The Base64 Encoding Service for JWTs</param>
+		/// <param name="decoder">The Decoding Service for JWTs</param>
+		/// <param name="algorithm">The Encryption Algorith for JWTs</param>
+		/// <param name="encoder">The Encoding Service for JWTs</param>
+		/// <param name="loggingService">The logging service for errors, default null - no logging</param>
+		public JwtService(IJsonSerializer serializer, IDateTimeProvider provider, IJwtValidator validator,
 			IBase64UrlEncoder urlEncoder, IJwtDecoder decoder, IJwtAlgorithm algorithm, IJwtEncoder encoder,
-			ILoggingService loggingService = null)
+			ILoggingService loggingService = null, string secret = null, SecureString secureSecret = null)
 		{
 			Serializer = serializer;
 			Provider = provider;
@@ -23,19 +36,35 @@ namespace StandardDot.Authentication.Jwt
 			Encoder = encoder;
 			LoggingService = loggingService;
 			_secret = secret;
+			_secureSecret = secureSecret;
 		}
 
-		private string _secret;
-		private string Secret => _secret;
-		private IJsonSerializer Serializer { get; set; }
-		private IDateTimeProvider Provider { get; set; }
-		private IJwtValidator Validator { get; set; } 
-		private IBase64UrlEncoder UrlEncoder { get; set; }
-		private IJwtDecoder Decoder { get; set; }
-		private IJwtAlgorithm Algorithm { get; set; }
-		private IJwtEncoder Encoder { get; set; }
-		private ILoggingService LoggingService { get; set; }
+		private SecureString _secureSecret;
 
+		private string _secret;
+
+		protected virtual string Secret => _secret ?? _secureSecret.;
+		
+		protected virtual IJsonSerializer Serializer { get; }
+		
+		protected virtual IDateTimeProvider Provider { get; }
+		
+		protected virtual IJwtValidator Validator { get; } 
+		
+		protected virtual IBase64UrlEncoder UrlEncoder { get; }
+		
+		protected virtual IJwtDecoder Decoder { get; }
+		
+		protected virtual IJwtAlgorithm Algorithm { get; }
+		
+		protected virtual IJwtEncoder Encoder { get; }
+		
+		protected virtual ILoggingService LoggingService { get; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name=""></param>
 		public T DecodeJwtPayload<T>(string jwt)
 		{
 			try
@@ -50,6 +79,10 @@ namespace StandardDot.Authentication.Jwt
 			catch (SignatureVerificationException ex)
 			{
 				LoggingService?.LogException(ex, "Token has invalid signature", LogLevel.Info);
+			}
+			catch (Exception ex)
+			{
+				LoggingService?.LogException(ex, "Decoding Jwt threw an exception", LogLevel.Error);
 			}
 			return default(T);
 		}
