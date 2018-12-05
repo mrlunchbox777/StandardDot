a8deee465391b5e9a3d7bdbc6bcc16be0895b9c7
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-12-04 22:32:10 -0700

    Updating IEnumerableExtensions with documentation

--- src/CoreExtensions/IEnumerableExtensions.cs
+++ src/CoreExtensions/IEnumerableExtensions.cs
@@ -5,6 +5,14 @@ namespace CoreExtensions
 {
 	public static class IEnumerableExtensions
 	{
+		/// <summary>
+		/// Returns an Enumerable that is distinct by the function given
+		/// </summary>
+		/// <typeparam name="TSource">The type in the source <see cref="IEnumerable" /></typeparam>
+		/// <typeparam name="TKey">They type of the hash for each element</typeparam>
+		/// <param name="source">The source IEnumerable</param>
+		/// <param name="keySelector">The function that returns a unique hash for each element</param>
+		/// <returns>The filtered IEnumerable</returns>
 		public static IEnumerable<TSource> DistinctBy<TSource, TKey>
 			(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
 		{
@@ -16,6 +24,17 @@ namespace CoreExtensions
 					yield return element;
 				}
 			}
+		}
+		
+		/// <summary>
+		/// Returns an Enumerable that is distinct by the <see cref="GetHashCode" />
+		/// </summary>
+		/// <typeparam name="TSource">The type in the source <see cref="IEnumerable" /></typeparam>
+		/// <param name="source">The source IEnumerable</param>
+		/// <returns>The filtered IEnumerable</returns>
+		public static IEnumerable<TSource> Distinct<TSource>(this IEnumerable<TSource> source)
+		{
+			return DistinctBy(source, x => x.GetHashCode());
 		}
 	}
 }
