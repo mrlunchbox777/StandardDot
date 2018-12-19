fa24bd6d5c1b5ccfae4e736624947ce8602ffe8b
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-12-18 23:58:14 -0700

    WIP: Documentation Redis collection

--- src/CachingRedis/DataStructures/RedisLazyCollection.cs
+++ src/CachingRedis/DataStructures/RedisLazyCollection.cs
@@ -7,8 +7,14 @@ using StandardDot.CoreExtensions;
 
 namespace StandardDot.Caching.Redis.DataStructures
 {
+	/// <summary>
+	/// The lazy collection that represents a redis dataset
+	/// </summary>
+	/// <typeparam name="T">The type of object in the collection</typeparam>
 	public class RedisLazyCollection<T> : BaseLazyCollection<T>
 	{
+		/// <param name="source">The result of the query that will be lazily loaded</param>
+		/// <param name="service">The redis service that made the query</param>
 		public RedisLazyCollection(IEnumerable<T> source, RedisCachingService service)
 			: base(source)
 		{
@@ -16,13 +22,27 @@ namespace StandardDot.Caching.Redis.DataStructures
 			Source = _source;
 		}
 
+		/// <summary>
+		/// The result of the query that will be lazily loaded
+		/// </summary>
 		protected virtual IEnumerable<T> Source { get; set; }
 
+		/// <summary>
+		/// The redis service that made the query
+		/// </summary>
 		protected virtual RedisCachingService Service { get; }
 
 		// TODO: slow and bad... is there a better way?
+		/// <summary>
+		/// DON'T USE THIS,	Gets the count of the query, likely slow
+		/// </summary>
+		/// <returns>The count of the result of the query, likely slow</returns>
 		public override int Count => Source.Count();
 
+		/// <summary>
+		/// If the redis set is readonly, always false
+		/// </summary>
+		/// <returns>If the redis set is readonly, always false</returns>
 		public override bool IsReadOnly => false;
 
 		public override void Add(T item)
@@ -32,6 +52,9 @@ namespace StandardDot.Caching.Redis.DataStructures
 			Service.Cache(castedItem.Id, item);
 		}
 
+		/// <summary>
+		/// DON'T USE THIS,	Gets the result of the query and removes them one by one
+		/// </summary>
 		public override void Clear()
 		{
 			while (Source.AnySafe())
@@ -44,17 +67,32 @@ namespace StandardDot.Caching.Redis.DataStructures
 			}
 		}
 
+		/// <summary>
+		/// DON'T USE THIS,	Checks if the result set of query contains the item
+		/// </summary>
+		/// <param name="item">The item that the result set may or may not contain</param>
+		/// <returns>If the result set contains the item</returns>
 		public override bool Contains(T item)
 		{
 			return Source.Contains(item);
 		}
 
 		// slow and bad, but no better way
+		/// <summary>
+		/// DON'T USE THIS, Copies the result set to the array
+		/// </summary>
+		/// <param name="array">The array to copy to</param>
+		/// <param name="arrayIndex">The index of the array</param>
 		public override void CopyTo(T[] array, int arrayIndex)
 		{
 			Source.ToArray().CopyTo(array, arrayIndex);
 		}
 
+		/// <summary>
+		/// Removes an item from redis
+		/// </summary>
+		/// <param name="item">The item to remove</param>
+		/// <returns>If the item was removed</returns>
 		public override bool Remove(T item)
 		{
 			IRedisCachedObject castedItem = GetCastedItem(item, false);
@@ -66,6 +104,14 @@ namespace StandardDot.Caching.Redis.DataStructures
 			return Service.Invalidate(castedItem.Id);
 		}
 
+		/// <summary>
+		/// Removes an item from the collection
+		/// </summary>
+		/// <param name="item">The item to remove</param>
+		/// <param name="item">The item to remove</param>
+		/// <exception cref="System.InvalidOperationException">Unable to add an item to redis that doesn't
+		/// derive from <see cref="StandardDot.Caching.Redis.Abstract.IRedisCachedObject" /></exception>
+		/// <returns>If the item was removed</returns>
 		protected virtual IRedisCachedObject GetCastedItem(T item, bool throwException = true)
 		{
 			IRedisCachedObject castedItem = item as IRedisCachedObject;
