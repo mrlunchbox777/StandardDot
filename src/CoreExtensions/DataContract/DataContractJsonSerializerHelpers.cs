b1c1595e858a648fdeca88bf0a6d011e87c73b31
LAPTOP-P6CMP9KB\Jaunty <mrlunchbox777@gmail.com>
2018-12-18 22:13:38 -0700

    More fixes for the DataContractJsonSerializer generation method

--- src/CoreExtensions/DataContract/DataContractJsonSerializerHelpers.cs
+++ src/CoreExtensions/DataContract/DataContractJsonSerializerHelpers.cs
@@ -2,16 +2,34 @@ using System;
 using System.Linq;
 using System.Runtime.Serialization.Json;
 using StandardDot.Abstract.CoreServices;
+using StandardDot.CoreExtensions;
 
 namespace CoreExtensions.DataContract
 {
+	/// <summary>
+	/// Extensions for <see cref="ISerializationSettings" />
+	/// </summary>
 	public static class DataContractJsonSerializerHelpers
 	{
+		/// <summary>
+		/// Creates a <see cref="DataContractJsonSerializer" />
+		/// </summary>
+		/// <param name="serializationSettings">The settings to use to create the <see cref="DataContractJsonSerializer" /></param>
+		/// <exception cref="System.InvalidOperationException">
+		/// <see cref="DataContractJsonSerializer" /> does not support a <see cref="DataContractResolver" />
+		/// </exception>
+		/// <returns>A new <see cref="DataContractJsonSerializer" /></returns>
 		public static DataContractJsonSerializer GetSerializer<T>(ISerializationSettings serializationSettings)
 		{
+			if (serializationSettings == null)
+			{
+				return new DataContractJsonSerializer(typeof(T));
+			}
+
 			DataContractJsonSerializer ser = null;
-			bool knownTypesExist = serializationSettings?.KnownTypes?.Any() ?? false;
-			bool resolverExists = serializationSettings?.Resolver != null;
+			bool knownTypesExist = serializationSettings.KnownTypes.AnySafe();
+			bool resolverExists = serializationSettings.Resolver != null;
+
 			if (!knownTypesExist && !resolverExists)
 			{
 				ser = new DataContractJsonSerializer(typeof(T));
@@ -24,6 +42,7 @@ namespace CoreExtensions.DataContract
 			{
 				ser = new DataContractJsonSerializer(typeof(T), serializationSettings.KnownTypes);
 			}
+
 			return ser;
 		}
 	}
