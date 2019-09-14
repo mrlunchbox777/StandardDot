0b7a0b3a53368191fffddd4f99fe0dd4a3ee859b
Andrew Shoell <mrlunchbox777@gmail.com>
2019-09-14 12:17:01 -0600

    WIP: getting sonar scanner in

--- src/.buildscripts/test.sh
+++ src/.buildscripts/test.sh
@@ -1,15 +1,37 @@
+# build the project to get everything cached
 dotnet build ./StandardDot.sln
 
+# run the tests
 for i in $(ls -d1 *Tests/)
 do
-    # still need to put in sonarqube
     echo "-----------------------------------------------"
     echo "running $i"
     cd "$i"
-    dotnet test
+    dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
     cd ..
     echo "completed running $i"
     echo "-----------------------------------------------"
 done
 
-# tail -f /dev/null
+# run sonarqube
+if [ "${DSONAR_LOGIN}" != "" ]
+then
+    # sonar-scanner \
+    #     -Dsonar.projectKey=${DSONAR_PROJECTKEY} \
+    #     -Dsonar.organization=${DSONAR_ORGANIZATION} \
+    #     -Dsonar.sources=. \
+    #     -Dsonar.host.url=${DSONAR_HOST} \
+    #     -Dsonar.login=${DSONAR_LOGIN}
+    dotnet sonarscanner begin \
+        -d:sonar.projectKey=${DSONAR_PROJECTKEY} \
+        -d:sonar.organization=${DSONAR_ORGANIZATION} \
+        -d:sonar.sources=. \
+        -d:sonar.host.url=${DSONAR_HOST} \
+        -d:sonar.login=${DSONAR_LOGIN} \
+        -d:sonar.cs.opencover.reportsPaths="**/coverage.opencover.xml"
+        -d:sonar.coverage.exclusions="**Tests*.cs"
+    dotnet build ./StandardDot.sln
+    dotnet sonarscanner end
+else
+    echo "Skipping Sonar Scanner"
+fi
--- src/Test.Dockerfile
+++ src/Test.Dockerfile
@@ -1,5 +1,27 @@
 FROM microsoft/dotnet:2.2-sdk-alpine
 
+ARG SONAR_SCANNER_HOME="${HOME}/.sonar/sonar-scanner-${SONAR_SCANNER_VERSION}-linux"
+ARG SONAR_SCANNER_OPTS="-server"
+ARG SONAR_SCANNER_VERSION="4.0.0.1744"
+ARG INSTALL_SONAR_SCANNER="false"
+
+ENV SONAR_SCANNER_HOME="${SONAR_SCANNER_HOME}"
+ENV SONAR_SCANNER_OPTS="${SONAR_SCANNER_OPTS}"
+ENV SONAR_SCANNER_VERSION="${SONAR_SCANNER_VERSION}"
+
+ENV PATH="${SONAR_SCANNER_HOME}/bin:${PATH}"
+
+WORKDIR /tempApp
+COPY ./.buildscripts ./.buildscripts
+RUN    echo "SONAR_SCANNER_VERSION - ${SONAR_SCANNER_VERSION}" \
+    && echo "SONAR_SCANNER_HOME - ${SONAR_SCANNER_HOME}" \
+    && echo "SONAR_SCANNER_OPTS - ${SONAR_SCANNER_OPTS}" \
+    && echo "DSONAR_LOGIN - ${DSONAR_LOGIN}" \
+    && echo "INSTALL_SONAR_SCANNER - ${INSTALL_SONAR_SCANNER}" \
+    && echo "PATH - ${PATH}"
+RUN if [ "${INSTALL_SONAR_SCANNER}" == "true" ]; then ./.buildscripts/install_sonar_scanner.sh; else echo "Skipping Sonar Scanner"; fi
+# RUN ./.buildscripts/install_sonar_scanner.sh
+
 WORKDIR /tempApp
 COPY . .
 RUN ./.buildscripts/restore.sh
